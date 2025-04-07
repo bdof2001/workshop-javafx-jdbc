e33b6a8783a50b83f034a4d3f2c7ea5db8839093
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-08 00:42:19 +0100

    DepartmentList view design

--- src/main/java/nandes/workshop/controller/MainViewController.java
+++ src/main/java/nandes/workshop/controller/MainViewController.java
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("/nandes/workshop/gui/DepartmentList.fxml");
     }
 
     @FXML
